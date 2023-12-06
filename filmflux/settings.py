0914f437e5fa92898103128b6084e05308ca1b68
Enes Arici <enesarici12@outlook.com>
2023-12-06 23:33:43 +0000

    beginning of heroku

--- filmflux/settings.py
+++ filmflux/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = "DEVELOPMENT" in os.environ
 
-ALLOWED_HOSTS = ['8000-ennie0906-filmflux-w3w1wu28hvw.ws-eu106.gitpod.io']
+ALLOWED_HOSTS = ['8000-ennie0906-filmflux-w3w1wu28hvw.ws-eu106.gitpod.io','.herokuapp.com']
 
 
 # Application definition
@@ -57,6 +57,7 @@ INSTALLED_APPS = [
     'crispy_bootstrap5',
     'cloudinary',
     'cloudinary_storage',
+    
 ]
 
 
